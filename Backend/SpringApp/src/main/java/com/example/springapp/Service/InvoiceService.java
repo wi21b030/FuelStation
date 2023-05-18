a17c52a27b7a47104758034c4d766befa28d31de
wi21b030 <wi21b030@technikum-wien.at>
2023-05-18 18:39:57 +0200

    added Service functionality

--- Backend/SpringApp/src/main/java/com/example/springapp/Service/InvoiceService.java
+++ Backend/SpringApp/src/main/java/com/example/springapp/Service/InvoiceService.java
@@ -2,6 +2,23 @@ package com.example.springapp.Service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class InvoiceService {
+
+    private final String fileStoragePath = "..\\..\\..\\..\\..\\..\\..\\FileStorage\\";
+    public boolean createInvoice(int customerID) {
+        boolean requestSent = false;
+        // Queue Logic
+        return requestSent;
+    }
+
+    public List<String> getInvoice(int customerID) {
+        List<String> invoiceInfo = new ArrayList<String>();
+        invoiceInfo.add(fileStoragePath+"1.txt");
+        invoiceInfo.add("18.05.23");
+        return invoiceInfo;
+    }
 }
