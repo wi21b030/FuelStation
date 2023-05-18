712b3167ba102b462aee17ed370b9970ef93c120
wi21b030 <wi21b030@technikum-wien.at>
2023-05-18 22:11:57 +0200

    added formatted response for getInvoice

--- Backend/SpringApp/src/main/java/com/example/springapp/Service/InvoiceService.java
+++ Backend/SpringApp/src/main/java/com/example/springapp/Service/InvoiceService.java
@@ -2,9 +2,14 @@ package com.example.springapp.Service;
 
 import com.example.springapp.Queue.Queue;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,14 +24,24 @@ public class InvoiceService {
 
     public List<String> getInvoice(int customerID) {
         String fileStoragePath = ".\\Backend\\FileStorage\\";
-        String filePath = fileStoragePath + customerID + ".txt";
+        String filePath = fileStoragePath + customerID + ".pdf";
         Path path = Paths.get(filePath);
         if (Files.exists(path) && Files.isRegularFile(path)) {
             List<String> invoiceInfo = new ArrayList<>();
             invoiceInfo.add(filePath);
+            try {
+                FileTime creationTime = Files.getLastModifiedTime(path);
+                String formattedCreationTime = creationTime.toInstant()
+                        .atZone(ZoneId.systemDefault())
+                        .format(DateTimeFormatter.ofPattern("dd.MM.yyyy - HH:mm"));
+                invoiceInfo.add("Created: " + formattedCreationTime.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             return invoiceInfo;
         }
         return null;
     }
+
 
 }
